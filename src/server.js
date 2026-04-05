b71dfb8a342257159e34ba637fd4c29ed2ad6099
ShambaSaha <shambasaha498@gmail.com>
2026-04-05 12:38:39 +0530

    fix: render port issue

--- src/server.js
+++ src/server.js
@@ -4,4 +4,8 @@ const connectDB = require("./config/db");
 
 connectDB();
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
